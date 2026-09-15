1bc41a6c60e27f1f216620562ab56dc20251b3c4
vahid moniei <   GitHub >
2026-09-15 22:42:22 +0330

    Set radar age range to 4 hours-2 days and 15s snapshots

--- app-accumulation-v11.js
+++ app-accumulation-v11.js
@@ -18,7 +18,7 @@ const MIN_HOLDERS = 300;
 const MAX_HOLDERS = 10000;
 
 const MIN_AGE_DAYS = 0.1666666667;
-const MAX_AGE_DAYS = 1;
+const MAX_AGE_DAYS = 2;
 const WATCHLIST_FILE =
     path.join(__dirname, "radar-watchlist.json");
 
@@ -1883,8 +1883,11 @@ saved++;
 
 setInterval(
     takeSnapshot,
-    10 * 1000
+    15 * 1000
 );
 
 takeSnapshot();
+
+
+
 
